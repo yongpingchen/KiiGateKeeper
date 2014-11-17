dd6495cb78d9af8eef872713e4152cdf515202fc
Yongping Chen <yongping.chen@kii.com>
2014-11-17 16:32:32 +0900

    Init project

---
+++ KiiGateKeeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KiiGateKeeper
+//
+//  Created by Yongping on 11/17/14.
+//  Copyright (c) 2014 Kii Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KiiGateKeeper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KiiGateKeeper
+//
+//  Created by Yongping on 11/17/14.
+//  Copyright (c) 2014 Kii Corporation. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
